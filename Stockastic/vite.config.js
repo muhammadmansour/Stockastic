455deebc2de4d54d7cf720a84740a0963a5d46fb
muhammadmansour <58367277+muhammadmansour@users.noreply.github.com>
2025-07-26 22:45:29 +0300

    modify endpoints

--- Stockastic/vite.config.js
+++ Stockastic/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
  server: {
     proxy: {
       '/api': {
-        target: 'http://newsapi.stockastic.app',
+        target: 'https://newsapi.stockastic.app',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
